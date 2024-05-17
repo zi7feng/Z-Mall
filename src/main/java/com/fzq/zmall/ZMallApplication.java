d968e8f23a7e3ee2408dd1414ac4b9b11966fc03
zfeng <zi7feng7@gmail.com>
2024-05-17 17:52:40 -0400

    initial commit

---
+++ src/main/java/com/fzq/zmall/ZMallApplication.java
@@ -0,0 +1,13 @@
+package com.fzq.zmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fzq/zmall/ZMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fzq.zmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
